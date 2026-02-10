84650fe7e2ddc337f4d8bca9149dfd09ba9a41e9
Estivendelgado16 <brallamdelgado351@gmail.com>
2026-02-10 11:49:27 -0500

    fixed bug

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,7 +4,7 @@ const { getUsers, saveUsers, upDateUser, deleteUser } = require('../controllers/
 
 
 //logic routes
-router.get('/', getUsers);
+router.get('/users', getUsers);
 router.post('/', saveUsers);
 router.put('/:id', upDateUser);
 router.delete('/:id', deleteUser);
